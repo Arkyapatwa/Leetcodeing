80179127c189fec3bc55779571de9213f87fb0a3
Arkya Patwa <59512209+Arkyapatwa@users.noreply.github.com>
2022-09-29 16:14:14 +0530

    Time: 532 ms (25.10%), Space: 13.5 MB (95.68%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.py
+++ 47-permutations-ii/47-permutations-ii.py
@@ -1,15 +1,18 @@
-class Solution:
+class Solution(object):
     def permute(self,indx,nums,arr):
         if len(nums)==indx and nums not in arr:
             arr.append(list(nums))
             return
         
         for i in range(indx,len(nums)):
+            if i>indx and nums[i]==nums[i-1]:
+                continue
             nums[i],nums[indx]=nums[indx],nums[i]
             self.permute(indx+1,nums,arr)
             nums[i],nums[indx]=nums[indx],nums[i]
-                
-    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+            
+    def permuteUnique(self, nums):
         arr=[]
         self.permute(0,nums,arr)
         return arr
+        
